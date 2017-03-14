06e3f21e74f269f8bc3198e7c2157c18356af705
Jack <lijie9228@hotmail.com>
2017-03-14 17:02:25 +0800

    初始化项目

---
+++ mysite/music/admin.py
@@ -0,0 +1,9 @@
+
+# Register your models here.
+
+from django.contrib import admin
+from .models import Album, Song
+
+admin.site.register(Album)
+admin.site.register(Song)
+
---
+++ mysite/music/urls.py
@@ -0,0 +1,16 @@
+
+from django.conf.urls import url
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+
+    url(r'^$', views.index, name='index'),
+    # music/<album_id>/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # music/<album_id>/favourite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+]
+

af971473c6be14bcce5868ad4ac76c66a1cf74cd
Jack <lijie9228@hotmail.com>
2017-08-25 16:39:10 +0800

    大改 数据库操作前保存

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -6,33 +6,45 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 
+class Attribute(models.Model):
+    name = models.CharField(max_length=250)
 
 
-
-class Size(models.Model):
-    title = models.CharField(max_length=250)
-
-
-class Color(models.Model):
-    title = models.CharField(max_length=250)
+class Attribute_value(models.Model):
+    name = models.CharField(max_length=250)
+    a_id = models.CharField(max_length=250,default='0')
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=250)
+    name = models.CharField(max_length=250,default='0')
 
+
+class Category_value(models.Model):
+    name = models.CharField(max_length=250)
+    c_id = models.CharField(max_length=250,default='0')
     # def __str__(self):
     #     return self.title 
 
 
 class Goods(models.Model):
+    common_id = models.CharField(max_length=250, default='0')
     name = models.CharField(max_length=250)
     goods_pic = models.CharField(max_length=250)
-    stock = models.CharField(max_length=250, default='99')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE,default='0')
+    stock = models.CharField(max_length=250, default='99')#库存
+    categorys = models.TextField(default='null')#'c_id':'c_name',
+    attributes = models.TextField(default='null')
 
     def __str__(self):
         return self.name 
 
-    def get_absolute_url(self):
-        return reverse('shop:detail', kwargs={'pk': self.pk})
+    # def get_absolute_url(self):
+    #     return reverse('shop:detail', kwargs={'id': self.common_id})
 
+
+class Common(models.Model):
+    name = models.CharField(max_length=250)
+    goods_pics = models.CharField(max_length=250)
+    categorys = models.TextField(default='null')
+    attributes = models.TextField(default='null')
+    def_goods_id = models.CharField(max_length=250, default='0')
+    goods_ids = models.CharField(max_length=250, default='0')
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -2,15 +2,60 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
+from django.template import RequestContext  
+from django.http import HttpResponse
+# from django.template import loader
 from django.views import generic
 from django.views.generic import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
-from .models import Goods
-from .models import Category
+from .models import Goods, Common, Category, Attribute
+# from .models import Category
+
+import pickle
+import json
 
 # Create your views here.
+
+
+#乱鸡巴写的取出数组也写对了卧槽
+def newlist(l):
+    nlist = []
+    for d in l:
+        for key in d :
+            if key == '1':
+                nlist.append (d[key])
+    return nlist
+        
+
+
+def add_goods_get(request):
+    return render_to_response('shop/addgoods.html')
+
+
+
+def add_goods(request):
+    return render_to_response('shop/addgoods.html')
+
+
+def goods_detail(request, goods_id):
+    goods = Goods.objects.get(id=goods_id)
+    attributes = json.loads(goods.attributes)
+    sizes = newlist(attributes)
+    context = {
+        'goods':goods,
+        'attributes':attributes,
+        'sizes':sizes
+    }
+    return render(request,'shop/detail.html',context)
+
+# def goodsdetail(request,goods_id):
+#     goods = Goods.objects.get(id=goods_id)
+#     # template_name = 'shop/detail.html'
+#     return render(request,'shop/detail.html',{'attributes':goods.attributes})
+
 
 class IndexView(generic.ListView):
     template_name = 'shop/index.html'
@@ -20,14 +65,16 @@ class IndexView(generic.ListView):
         return Goods.objects.all()
 
 
-class DetailView(generic.DetailView):
-    model = Goods
-    template_name = 'shop/detail.html'
-
-
-# class GoodsCreate(CreateView):
+# class DetailView(generic.DetailView):
 #     model = Goods
-#     fields = ['name', 'goods_pic']
+#     template_name = 'shop/detail.html'
+
+#     def get_attribute():
+#         return json.loads(Goods.attributes);
+
+class GoodsCreate(CreateView):
+    model = Goods
+    fields = ['name', 'goods_pic']
 
 
 # class GoodsUpdate(UpdateView):
